971ab60873f8913f117e04e2858350272ba105af
Renjithkumar993 <121647888+Renjithkumar993@users.noreply.github.com>
2023-03-31 23:08:00 -0300

    sa

---
+++ server.js
@@ -0,0 +1,28 @@
+const cTable = require('console.table');
+const inquirer =  require("inquirer")
+const fs = require("fs")
+const mysql = require('mysql2');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("%c------------------------------------------------", "background-color: #FDB813; color: #FFFFFF; font-size: 1px; height: 10px;");
+console.log("%c    Welcome to the SVG Logo Maker App    ", "background-color: #FDB813; color: #FFFFFF; font-size: 36px; font-weight: bold; padding: 20px; text-align: center;");
+console.log("%c------------------------------------------------", "background-color: #FDB813; color: #FFFFFF; font-size: 1px; height: 10px;");
+console.log("%cCreate professional-quality logos in seconds!", "color: #262626; font-size: 18px; font-weight: bold; margin-top: 30px; text-align: center;");
+console.log("%cOur intuitive interface makes logo design a breeze.", "color: #262626; font-size: 16px; margin-top: 20px; margin-bottom: 20px; text-align: center;");
+console.log("%cWith a wide range of customization options, you can create logos that truly reflect your brand.", "color: #262626; font-size: 16px; text-align: center;");
+console.log("%c------------------------------------------------", "background-color: #FDB813; color: #FFFFFF; font-size: 1px; height: 10px;");
+
+;
